18484a5787fa593218ddb4fd59f217292569ff53
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-09 18:23:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,7 +19,11 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-  if position_taken?(board,index)
+  if index == 0 && board == [" "]
+    return true
+  elsif index <= 9
+    return false
+  else position_taken?(board,index)
     return false
   end
 end
